f7a2e80449419ef872c6ca3d6bd699d03eecc3b5
samyarj <112454395+samyarj@users.noreply.github.com>
2022-10-18 20:39:34 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -19,8 +19,17 @@ def join_integers(numbers: list[int]) -> int:
 def generate_prime_numbers(limit):
 	return [0]
 
-def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+def combine_strings_and_numbers(strings, num_combinations, excluded_multiples) -> list[str]:
+	result = []
+
+	for i in range(1, num_combinations+1):
+		if excluded_multiples == None:
+			result.append(strings[0] + str(i))
+			result.append(strings[1] + str(i))
+		elif i % excluded_multiples != 0:
+			result.append(strings[0] + str(i))
+			result.append(strings[1] + str(i))
+	return result
 
 if __name__ == "__main__":
 	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
